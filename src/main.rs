1b450d663779553c162d9fe932c6aee789ecd171
William Speirs <bill.speirs@gmail.com>
2019-01-21 14:51:08 -0500

    Got matching output to working gr

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,160 @@
 #[macro_use]
 extern crate log;
 
+use std::cell::RefCell;
+use std::rc::Rc;
 use std::time::Duration;
+use std::thread::sleep;
+use std::io::{Write, stdout, BufWriter};
+use std::fs::{File, OpenOptions};
 
-use libusb::{Context, DeviceHandle};
-use libusb::Error as USBError;
+use byteorder::{LE, WriteBytesExt};
 
-mod rtlsdr;
+use std::f32::consts::PI;
 
-/// Goes through the devices until it finds a match for the product_id and vendor_id, then opens it
-fn open_device(context: &Context, pid: u16, vid: u16) -> Result<DeviceHandle, USBError> {
-    for mut device in context.devices().unwrap().iter() {
-        let device_desc = device.device_descriptor().unwrap();
+use num::complex::Complex32;
 
-        debug!("DEVICE: {:#06x}:{:#06x}", device_desc.product_id(), device_desc.vendor_id());
+use rs_libhackrf::hackrf::HackRF;
+use rs_libhackrf::error::Error;
 
-        if device_desc.product_id() != pid || device_desc.vendor_id() != vid {
-            continue;
-        }
+mod fm;
 
-        return device.open();
-    }
+use fm::FmDemod;
 
-    return Err(USBError::NoDevice);
-}
+const FREQ :u64 = 95_900_000; // set to 95.9MHz
+const SAMPLE_RATE :u32 = 10_000_000;
 
-fn main() -> Result<(), USBError> {
-    simple_logger::init_with_level(log::Level::Debug).unwrap();
+fn main() -> Result<(), Error> {
+//    simple_logger::init_with_level(log::Level::Debug).unwrap();
 //    simple_logger::init_with_level(log::Level::Info).unwrap();
 
-    let mut context = libusb::Context::new()?;
+    let mut hrf = HackRF::new().unwrap();
+    let mut dev = hrf.open_device(0).expect("Error opening device; not plugged in?");
 
-    // open the SDR
-    let mut device_handle = open_device(&context, 0x2838, 0x0bda)?;
+    dev.set_vga_gain(20)?;
+    dev.set_lna_gain(20)?;
+    dev.set_amp_enable(false);
 
-    // claim the interface
-    device_handle.claim_interface(0)?;
+    dev.set_antenna_enable(true);
 
-    // reset the USB device just in case
-    device_handle.reset();
+    dev.set_freq(FREQ)?;
+    dev.set_sample_rate(SAMPLE_RATE as f64)?;
 
-    //
-    // RTL-SDR specific code
-    //
-    rtlsdr::init_baseband(&mut device_handle).expect("Error setting the baseband");
+    let mut demod = Rc::new(RefCell::new(FmDemod::new(75000, SAMPLE_RATE)));
+    let mut demod_clone = demod.clone();
 
-//    rtlsdr_set_i2c_repeater(dev, 1);
+    let mut mag_file = BufWriter::new(OpenOptions::new().write(true).create(true).open("my_mag.dat").expect("Cannot open mag file"));
+    let mut phase_file = BufWriter::new(OpenOptions::new().write(true).create(true).open("my_phase.dat").expect("Cannot open phase file"));
+    let mut test_file = BufWriter::new(OpenOptions::new().write(true).create(true).open("my_test.dat").expect("Cannot open phase file"));
 
+//    let mut mag_file = Rc::new(RefCell::new(OpenOptions::new().write(true).create_new(true).open("my_mag.dat")));
+//    let mut phase_file = Rc::new(RefCell::new(OpenOptions::new().write(true).create_new(true).open("my_phase.dat")));
+//    let mut mag_file_clone = mag_file.clone();
+//    let mut phase_file = phase_file.clone();
+
+    dev.start_rx(|buff| {
+        let mut demod = demod_clone.borrow_mut();
+        let mut std_out = stdout();
+
+//        buff.iter().for_each(|b| {
+//            test_file.write_u8(*b);
+//        });
+//
+//        test_file.flush();
+
+        let buff = buff.chunks(2).map(|chunk| {
+            Complex32::new(chunk[0] as f32 / 128.0, chunk[1] as f32 / 128.0)
+        }).collect::<Vec<_>>();
+
+
+        buff.iter().for_each(|c| {
+            test_file.write_f32::<LE>(c.re);
+            test_file.write_f32::<LE>(c.im);
+        });
+
+        test_file.flush();
+
+//        buff.iter().for_each(|c| {
+//            mag_file.write_f32::<BigEndian>(c.re);
+//            phase_file.write_f32::<BigEndian>(c.im);
+//        });
+//
+//        mag_file.flush();
+//        phase_file.flush();
+
+//        let buff = buff.chunks(8).map(|chunk| {
+//            vec![
+//                Complex32::new((chunk[0] as f32) - 127.5, (chunk[1] as f32) - 127.5),
+//                Complex32::new(((255 - chunk[3]) as f32) - 127.5, (chunk[2] as f32) - 127.5),
+//                Complex32::new(((255 - chunk[4]) as f32) - 127.5, ((255 - chunk[5]) as f32) - 127.5),
+//                Complex32::new((chunk[7] as f32) - 127.5, ((255 - chunk[6]) as f32) - 127.5)
+//            ]
+//        }).flatten().collect::<Vec<_>>();
+//
+//        // sum up the values in chunks of 6; downsampling
+//        let buff = buff.chunks(6).map(|chunk| {
+//            chunk.into_iter().sum()
+//        });
+//
+//        // do the demodulation
+//        let mut buff = buff
+//            .map(|c| demod.feed(c))
+//            .map(|f| (f / PI * (1<<14) as f32) as i16)
+//            .collect::<Vec<_>>()
+//        ;
+//
+//        // deemph filter???
+//
+//        // low pass
+//        let i2 :isize = 0;
+//        let fast :isize = 170000; // rate_out
+//        let slow :isize = 32000;  // rate_out2
+//        let mut now_lpr :isize = 0;
+//        let mut pre_lpr_index :isize = 0;
+//        let mut ret_buff = Vec::new();
+//
+//        for res in buff {
+//            now_lpr += res as isize;
+//            pre_lpr_index += slow;
+//
+//            if pre_lpr_index < fast {
+//                continue
+//            }
+//
+//            ret_buff.push((now_lpr / (fast/slow)) as i16);
+//            now_lpr = 0;
+//            pre_lpr_index -= fast;
+//        }
+//
+//        ret_buff.into_iter().for_each(|v| {
+//            std_out.write_i16::<BigEndian>(v);
+//        });
+
+        Error::SUCCESS
+//        Error::INVALID_PARAM(String::new())
+    })?;
+
+    sleep(Duration::from_secs(3));
 
     Ok(())
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_zip() {
+        let buff = (0..20).collect::<Vec<u8>>();
+
+        let i = buff.iter().step_by(2).map(|n| *n);
+        let q = buff.iter().skip(1).step_by(2).map(|n| *n);
+
+        for (i,q) in i.zip(q) {
+            println!("I: {} Q: {}", i, q);
+        }
+    }
+
 }
 
